b701d7313e9989d1658a6286326e1c8d7dd306ad
DanielSilvado <danielsilvado@id.uff.br>
2020-06-17 23:59:41 -0300

    initial form create points

--- src/pages/CreatePoints/index.tsx
+++ src/pages/CreatePoints/index.tsx
@@ -18,6 +18,51 @@ const CreatePoint = () => {
         </Link>
       </header>
 
+      <form >
+        <h1>Cadastro do <br /> ponto de coleta</h1>
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <div className="field">
+            <label htmlFor="name">Nome da entidade</label>
+            <input type="text"
+              name="name"
+              id="name"
+            />
+          </div>
+
+          <div className="field-group">
+            <div className="field">
+              <label htmlFor="email">Email</label>
+              <input type="email"
+                name="email"
+                id="email"
+              />
+            </div>
+            <div className="field">
+              <label htmlFor="whatsapp">Whatsapp</label>
+              <input type="text"
+                name="whatsapp"
+                id="whatsapp"
+              />
+            </div>
+          </div>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+          </legend>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Ítens de coleta</h2>
+          </legend>
+        </fieldset>
+      </form>
 
     </div>
   )
