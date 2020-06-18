8bad0eafe297120e5aec048ad3d84f975780510b
DanielSilvado <danielsilvado@id.uff.br>
2020-06-17 23:53:44 -0300

    header create points

--- src/pages/CreatePoints/index.tsx
+++ src/pages/CreatePoints/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { FiArrowLeft } from 'react-icons/fi'
+
+import '.styles.css'
+
+import logo from '../../assets/logo.svg'
 
 const CreatePoint = () => {
   return (
-    <h1>Hello Planet</h1>
+    <div id="page-create-poits">
+      <header>
+        <img src={logo} alt="" />
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para home
+        </Link>
+      </header>
+
+
+    </div>
   )
 }
 
